bc005859a9b2cf46d18a7d5bd1907c13a6db2ba3
swtor-miner <swtorminer@gmail.com>
2015-04-02 17:28:16 -0400

    Torhead scanner + item url output.

--- PugTools/ExtractionFunctions/Torcommunity.cs
+++ PugTools/ExtractionFunctions/Torcommunity.cs
@@ -34,7 +34,9 @@ namespace tor_tools
 
             //getAuctionCats();
 
-            getitemIds();
+            //getitemIds();
+
+            torheadscanner();
             
             EnableButtons();
         }
@@ -72,6 +74,84 @@ namespace tor_tools
                 WriteFile(String.Format("{0}: http://torcommunity.com/db/{1}{2}", itm.Name, itm.Base62Id, Environment.NewLine), "itemIds.txt", true);
                 i++;
             }
+        }
+
+        public void torheadscanner()
+        {
+            //http://www.torhead.com/item/ace+in+the+hole
+            //http://www.torhead.com/item/schematic:+[artifact]+microfilament+skill+d-device
+            //http://www.torhead.com/item/agent's-birthright-headgear
+
+            ClearProgress();
+            LoadData();
+            var gomList = currentDom.GetObjectsStartingWith("itm.");
+            var count = gomList.Count();
+            int i = 0;
+            Dictionary<string, GomLib.Models.Item> itemlist = new Dictionary<string, GomLib.Models.Item>();
+            foreach (var gom in gomList)
+            {
+                progressUpdate(i, count);
+                var itm = currentDom.itemLoader.Load(gom);
+                if (itemlist.ContainsKey(itm.Name))
+                {
+                    //add code here to prioritize items based on quality/item level
+                }
+                else if(itm.Name != null && itm.Name != ""){
+                    itemlist.Add(itm.Name, itm);
+                }
+                i++;
+            }
+            addtolist("item list created in memory, scanning torhead");
+            addtolist(String.Format("Found {0} unique names, full scan", itemlist.Count));
+            TimeSpan t = TimeSpan.FromSeconds(itemlist.Count/5);
+
+            string answer = String.Format("will take {0:D2}h:{1:D2}m:{2:D2}s",
+                            t.Hours,
+                            t.Minutes,
+                            t.Seconds);
+            addtolist(answer);
+            Dictionary<string, string> nametourlmap = new Dictionary<string, string>();
+            WriteFile("", "torheadurls.txt", false);
+            WriteFile("", "badtorheadurls.txt", false);
+            WriteFile("", "torheadurlserrors.txt", false);
+
+            ClearProgress();
+            count = itemlist.Count();
+            i = 0;
+            EnableButtons();
+            foreach (var kvp in itemlist)
+            {
+                progressUpdate(i, count);
+                string url = String.Format("http://www.torhead.com/item/{0}", kvp.Key.Replace(" ", "+"));
+                HttpWebRequest req = (HttpWebRequest)HttpWebRequest.CreateHttp(url);
+                req.AllowAutoRedirect = false;
+                try
+                {
+                    var response = (HttpWebResponse)req.GetResponse();
+                    if (response.StatusCode != HttpStatusCode.InternalServerError)
+                    {
+                        string loc = response.GetResponseHeader("Location");
+                        nametourlmap.Add(kvp.Value.Base62Id, loc);
+                        WriteFile(String.Format("{0},{1},http://www.torhead.com{2}{3}", kvp.Value.Base62Id, url, loc, Environment.NewLine), "torheadurls.txt", true);
+                        addtolist2(url);
+                    }
+                    else
+                        WriteFile(String.Format("{0},{1}{2}", kvp.Value.Base62Id, url, Environment.NewLine), "badtorheadurls.txt", true);
+                }
+                catch (WebException ex)
+                {
+                    WriteFile(String.Format("{0},{1}{2}", kvp.Value.Base62Id, url, Environment.NewLine), "torheadurlserrors.txt", true);
+                    addtolist(String.Format("Error: {0}", ex.Message));
+                    if (ex.Status == WebExceptionStatus.ProtocolError)
+                    {
+                        addtolist(String.Format("Status Code : {0}", ((HttpWebResponse)ex.Response).StatusCode));
+                        addtolist(String.Format("Status Description : {0}", ((HttpWebResponse)ex.Response).StatusDescription));
+                    }
+                }
+                i++;
+                Thread.Sleep(200);
+            }
+            addtolist("Done scanning torhead!");
         }
     }
 }
