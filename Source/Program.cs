dd3f73d2a1a7850d2c3f0b553ae9a4beb7766f6b
Kyle Weierbach <aikiwoce@gmail.com>
2020-09-06 19:25:38 -0400

    Desktop scaling fix.

--- Source/Program.cs
+++ Source/Program.cs
@@ -11,9 +11,16 @@ namespace PugTools
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                SetProcessDPIAware();
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Tools());
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
