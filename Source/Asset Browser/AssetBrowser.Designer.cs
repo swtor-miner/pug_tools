547f5e4e217ff47f8c3ea024f0259b456651e232
Kyle Weierbach <aikiwoce@gmail.com>
2020-09-06 21:12:40 -0400

    AssetBrowser Image Preview scrolling fix

--- Source/Asset Browser/AssetBrowser.Designer.cs
+++ Source/Asset Browser/AssetBrowser.Designer.cs
@@ -265,7 +265,7 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.BackgroundImage = global::PugTools.Properties.Resources.Transparent;
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(701, 601);
